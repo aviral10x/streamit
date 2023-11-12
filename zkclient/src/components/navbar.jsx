0bae2416e75d6251b8a126c907b553345f6c67e2
AVIRAL GUPTA <91749530+aviral10x@users.noreply.github.com>
2023-11-12 11:08:08 +0300

    spelling

--- zkclient/src/components/navbar.jsx
+++ zkclient/src/components/navbar.jsx
@@ -1,28 +1,25 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   return (
     <nav className="bg-black p-4 flex justify-between">
       <div className="flex items-center">
-        <span className="text-white text-xl font-bold">Streame</span>
+        <span className="text-white text-xl font-bold">StreamIT</span>
       </div>
       <div className="flex items-center">
-          <button
-          className="bg-white text-black px-6 py-2 mx-2 rounded-md"
-        >
+        <button className="bg-white text-black px-6 py-2 mx-2 rounded-md">
           Mint Faucet Record
         </button>
         <button className="bg-white text-black px-6 py-2 mx-2 rounded-md">
           Stream Balance
         </button>
         <button className="bg-white text-black px-6 py-2 rounded-md">
-         <Link to={`/withdraw`}> Withdraw</Link>
+          <Link to={`/withdraw`}> Withdraw</Link>
         </button>
         <button className="bg-white text-black px-6 py-2 mx-2 rounded-md">
-         <Link to={`/cancel`}> Cancel</Link>
+          <Link to={`/cancel`}> Cancel</Link>
         </button>
-        </div>
+      </div>
     </nav>
   );
 };
